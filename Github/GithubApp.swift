b3830dd8e3161612949c3eac7808dc8b75f0b62a
May Bader <maibadr200@gmail.com>
2024-09-27 18:14:48 +0300

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by May Bader Alotaibi on 24/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
